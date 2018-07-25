757e848d97a4f26133c237219447c13d93583e2a
D-Bullean <36720788+D-Bullean@users.noreply.github.com>
2018-07-25 01:59:55 -0500

    upload

---
+++ Hashing.java
@@ -0,0 +1,15 @@
+
+
+import java.math.BigInteger;
+
+public class Hashing {
+	
+	 public BigInteger Hashing(BigInteger message){
+    	BigInteger modulus = new BigInteger("8");
+    	BigInteger hashedVal = message.mod(modulus);
+    	return hashedVal;
+    	
+    }
+    
+
+}
